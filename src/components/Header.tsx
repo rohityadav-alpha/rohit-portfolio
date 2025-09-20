675f1fb651df669fde6f7eaa0f5480ef54c965cb
rohityadav-next <yadavratan524@gmail.com>
2025-09-20 14:46:09 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const isHomePage = pathname === '/';
 
   return (
-    <header className=" w-30 h-16 shadow-lg sticky top-0 z-50 items-center justify-center backdrop-blur-md bg-white/1 ">
+    <header className=" w-30 h-16 shadow-lg sticky top-0 z-50 items-center justify-center bg-white ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
